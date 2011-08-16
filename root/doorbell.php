5e0caea0e59dcffa79df128201a031a68501c7a5
Kirill Kholodilin <charlie@chrl.ru>
2011-08-16 12:50:19 +0600

    Fixed spaces/tabs

--- root/doorbell.php
+++ root/doorbell.php
@@ -4,12 +4,12 @@
     
     $core = new WebCore();
     $core-> 
-	    setConfig(new Config('web.php'))->
-        setRouter(new UrlRouter())->
-        setDebugLevel(true)->
-	    attach('Cache')->
-        attach('HeaderOutput')->
-	    attach('TemplateOutput')->
-	    attach('Counter')->        
-	    process()->
-	    shutdown();
+		setConfig(new Config('web.php'))->
+		setRouter(new UrlRouter())->
+		setDebugLevel(true)->
+		attach('Cache')->
+		attach('HeaderOutput')->
+		attach('TemplateOutput')->
+		attach('Counter')->        
+		process()->
+		shutdown();
